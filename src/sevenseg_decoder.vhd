175541e513b3dde8ad4ae28e351fc0b439bb961d
Goth <c27cody.goth@afacademy.af.edu>
2025-03-04 14:27:24 -0700

    sevenseg_decoder.vhd completed

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -39,6 +39,23 @@ end sevenseg_decoder;
 architecture Behavioral of sevenseg_decoder is
 
 begin
-
+    with i_hex select
+        o_seg_n => NOT "0111111" when "0000", 
+                   NOT "0000110" when "0001",
+                   NOT "1001111" when "0010", 
+                   NOT "1001111" when "0011", 
+                   NOT "1100110" when "0100", 
+                   NOT "1101101" when "0101", 
+                   NOT "1111101" when "0110", 
+                   NOT "0000111" when "0111", 
+                   NOT "1111111" when "1000",
+                   NOT "1100111" when "1001", 
+                   NOT "1110111" when "1010", 
+                   NOT "1111100" when "1011", 
+                   NOT "1011000" when "1100", 
+                   NOT "1011110" when "1101", 
+                   NOT "1111001" when "1110", 
+                   NOT "1110001" when "1111", 
+                   "1111111" when others; 
 
 end Behavioral;
